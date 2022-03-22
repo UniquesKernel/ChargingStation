d6d9e69eff79363f8fb162530826c34b0052692f
Yevhen <yevhen89@gmail.com>
2022-03-22 22:57:30 +0100

    Rfid classes, simulator and test

---
+++ ChargingStationLibrary/IRfidReader.cs
@@ -0,0 +1,11 @@
+﻿namespace ChargingStationLibrary
+{
+    public class RfidEventArgs : EventArgs
+    {
+        public int Rfid { get; set; }
+    }
+    public interface IRfidReader
+    {
+        event EventHandler<RfidEventArgs> RfidDetected;
+    }
+}
---
+++ ChargingStationTest/TestRfidReaderSimulator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+using ChargingStationLibrary;
+
+namespace ChargingStationTest
+{
+    [TestFixture]
+    public class TestRfidReaderSimulator
+    {
+        private RfidReaderSimulator _uut;
+
+        public void Setup()
+        {
+            _uut = new RfidReaderSimulator();
+        }
+    }
+}
