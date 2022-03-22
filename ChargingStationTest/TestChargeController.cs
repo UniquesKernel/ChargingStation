e4092463e87a022e22bdd6c3bbf8ed7aed0ef9fb
Morten Høgsberg <morten-hoegsberg@protonmail.com>
2022-03-22 18:08:58 +0100

    testing webhook

--- ChargingStationTest/TestChargeController.cs
+++ ChargingStationTest/TestChargeController.cs
@@ -5,12 +5,10 @@ namespace ChargingStationTest;
 [TestFixture]
 public class TestChargeController
 {
-  private ChargeController _uut;
 
   [SetUp]
   public void SetUp()
   {
-    UsbChargerSimulator usbSim = new UsbChargerSimulator();
-    _uut = new ChargeController(usbSim);
+
   }
 }
